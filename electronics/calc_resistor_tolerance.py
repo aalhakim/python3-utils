3323c5dba00d914318f53a5a343e8459f3a74caf
Ali Al-Hakim <ali.al-hakim@brillpower.com>
2024-02-25 14:52:02 +0000

    Update calc_resistor_tolerance.py

--- electronics/calc_resistor_tolerance.py
+++ electronics/calc_resistor_tolerance.py
@@ -1,83 +1,105 @@
-#!python2
+#!/usr/bin/env python3
 
 """
 Calculate error in resistor potential divider networks.
 """
 
-RTOP = 10000.0
-RBOT = 10000.0
+###############################################################################
+# --- User Inputs
+###############################################################################
 
-VIN = 4.2
+# Top resistor value (in kOhms)
+RTOP = 124.0
+
+# Bottom resistor value (in kOhms)
+RBOT = 40.2
+
+# Input voltage (in Volts)
+VIN = 6.8
+
+# Ground reference voltage (in Volts), typically 0.0 V
 VGND = 0.00
 
-VFB  = 0.430435  # Relevant for DC/DC feedback pins
+# Set to True to calculate VFB from VIN.
+# Set to False to calculate VIN values from VFB
+CALCULATE_VFB_FROM_VIN = False
+
+# Feedback voltage level(s)
+#  VFB_MIN/MAX are relevant for DC-DC converters where the feedback reference
+#  has some error which may affect DC-DC regulation accuracy under some
+# conditions.
+VFB = 0.8
+VFB_TOL = None
+
+if VFB_TOL is not None:
+    VFB_MIN = VFB - VFB_TOL  # Use value or None
+    VFB_MAX = VFB + VFB_TOL  # Use value or None
+else:
+    VFB_MIN = None
+    VFB_MAX = None
 
 
-#######################################################################
+###############################################################################
+# --- Functions
+###############################################################################
 def pot_div_gain(rtop, rtop_tol, rbot, rbot_tol, gain):
-    rtop_min = rtop * (1 - (rtop_tol/100.0))
-    rtop_max = rtop * (1 + (rtop_tol/100.0))
-    rbot_min = rbot * (1 - (rbot_tol/100.0))
-    rbot_max = rbot * (1 + (rbot_tol/100.0))
+    rtop_min = rtop * (1 - (rtop_tol / 100.0))
+    rtop_max = rtop * (1 + (rtop_tol / 100.0))
+    rbot_min = rbot * (1 - (rbot_tol / 100.0))
+    rbot_max = rbot * (1 + (rbot_tol / 100.0))
 
-    if gain == 'nom':
+    if gain == "nom":
         return rbot_min / (rbot_min + rtop_min)
-    elif gain == 'max':
+    elif gain == "max":
         return rbot_max / (rbot_max + rtop_min)
-    elif gain == 'min':
+    elif gain == "min":
         return rbot_min / (rbot_min + rtop_max)
 
 
-#######################################################################
-min_gain = pot_div_gain(RTOP, 5, RBOT, 5, 'min')
-max_gain = pot_div_gain(RTOP, 5, RBOT, 5, 'max')
-nom_gain = pot_div_gain(RTOP, 5, RBOT, 5, 'nom')
+###############################################################################
+# --- Main Code
+###############################################################################
+if __name__ == "__main__":
+    print("  RTOP = {} Ohms".format(RTOP))
+    print("  RBOT = {} Ohms".format(RBOT))
 
-print("VIN  = {} V".format(VIN))
-print("RTOP = {} Ohms".format(RTOP))
-print("RBOT = {} Ohms".format(RBOT))
+    vfb_string = "\n"
+    if VFB_MIN is not None:
+        vfb_string += f"  VFB_MIN = {VFB_MIN:0.03f} V\n"
+    vfb_string += f"  VFB_NOM = {VFB:0.03f} V"
+    if VFB_MAX is not None:
+        vfb_string += f"\n  VFB_MAX = {VFB_MAX:0.03f} V"
 
-print("\nRESISTOR TOLERANCE 5%\n------------------------")
-print("  OUTPUT VOLTAGE for VIN=%0.02f" % VIN)
-print("    MAX = %0.06f V" % (min_gain * VIN))
-print("    NOM = %0.06f V" % (nom_gain * VIN))
-print("    MIN = %0.06f V" % (max_gain * VIN))
+    if CALCULATE_VFB_FROM_VIN:
+        print("\n   VIN = {} V".format(VIN))
+    else:
+        print(vfb_string)
 
-print("\n  INPUT VOLTAGE for VFB=%0.02f" % VFB)
-print("    MAX = %0.06f V" % ((1/min_gain)*VFB - ((1/min_gain)-1)*VGND))
-print("    NOM = %0.06f V" % ((1/nom_gain)*VFB - ((1/nom_gain)-1)*VGND))
-print("    MIN = %0.06f V" % ((1/max_gain)*VFB - ((1/max_gain)-1)*VGND))
+    if VFB_MIN is None:
+        VFB_MIN = VFB
 
+    if VFB_MAX is None:
+        VFB_MAX = VFB
 
-#######################################################################
-min_gain = pot_div_gain(RTOP, 1, RBOT, 1, 'min')
-max_gain = pot_div_gain(RTOP, 1, RBOT, 1, 'max')
-nom_gain = pot_div_gain(RTOP, 1, RBOT, 1, 'nom')
+    # -------------------------------------------------------------------------
+    for tolerance in [5, 1, 0.5, 0.1]:
+        min_gain = pot_div_gain(RTOP, tolerance, RBOT, tolerance, "min")
+        max_gain = pot_div_gain(RTOP, tolerance, RBOT, tolerance, "max")
+        nom_gain = pot_div_gain(RTOP, tolerance, RBOT, tolerance, "nom")
 
-print("\n\nRESISTOR TOLERANCE 1%\n------------------------")
-print("  OUTPUT VOLTAGE for VIN=%0.02f" % VIN)
-print("    MAX = %0.06f V" % (min_gain * VIN))
-print("    NOM = %0.06f V" % (nom_gain * VIN))
-print("    MIN = %0.06f V" % (max_gain * VIN))
+        print(f"\n  RESISTOR TOLERANCE {tolerance}%\n ------------------------")
+        if CALCULATE_VFB_FROM_VIN:
+            print("    VFB_MAX = %0.06f V" % (max_gain * VIN))
+            print("    VFB_NOM = %0.06f V" % (nom_gain * VIN))
+            print("    VFB_MIN = %0.06f V" % (min_gain * VIN))
 
-print("\n  INPUT VOLTAGE for VFB=%0.02f" % VFB)
-print("    MAX = %0.06f V" % ((1/min_gain)*VFB - ((1/min_gain)-1)*VGND))
-print("    NOM = %0.06f V" % ((1/nom_gain)*VFB - ((1/nom_gain)-1)*VGND))
-print("    MIN = %0.06f V" % ((1/max_gain)*VFB - ((1/max_gain)-1)*VGND))
+        else:
+            print(
+                "    VIN_MAX = %0.06f V" % ((1 / min_gain) * VFB_MAX - ((1 / min_gain) - 1) * VGND)
+            )
+            print("    VIN_NOM = %0.06f V" % ((1 / nom_gain) * VFB - ((1 / nom_gain) - 1) * VGND))
+            print(
+                "    VIN_MIN = %0.06f V" % ((1 / max_gain) * VFB_MIN - ((1 / max_gain) - 1) * VGND)
+            )
 
-
-#######################################################################
-min_gain = pot_div_gain(RTOP, 0.1, RBOT, 0.1, 'min')
-max_gain = pot_div_gain(RTOP, 0.1, RBOT, 0.1, 'max')
-nom_gain = pot_div_gain(RTOP, 0.1, RBOT, 0.1, 'nom')
-
-print("\n\nRESISTOR TOLERANCE 0.1%\n------------------------")
-print("  OUTPUT VOLTAGE for VIN=%0.02f" % VIN)
-print("    MAX = %0.06f V" % (min_gain * VIN))
-print("    NOM = %0.06f V" % (nom_gain * VIN))
-print("    MIN = %0.06f V" % (max_gain * VIN))
-
-print("\n  INPUT VOLTAGE for VFB=%0.02f" % VFB)
-print("    MAX = %0.06f V" % ((1/min_gain)*VFB - ((1/min_gain)-1)*VGND))
-print("    NOM = %0.06f V" % ((1/nom_gain)*VFB - ((1/nom_gain)-1)*VGND))
-print("    MIN = %0.06f V" % ((1/max_gain)*VFB - ((1/max_gain)-1)*VGND))
+    print()
